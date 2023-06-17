859e6325e979542cd38923ffa68b7bc0688cc18c
rzqthya <athiyya.fan178@gmail.com>
2023-06-17 17:38:16 +0700

    update fitur create

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -12,21 +12,52 @@ class EmployeeController extends Controller
 
     public function index()
     {
-      
+    
 
     }
 
 
     public function create()
     {
-        
+        $pageTitle = 'Create Employee';
+
+        // Eloquent
+        $positions = Position::all();
+
+        return view('employee.create', compact('pageTitle', 'positions'));
 
     }
 
 
     public function store(Request $request)
     {
-        
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email|unique:employees,email,',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // Eloquent
+        $employee = New Employee;
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+        return redirect()->route('employees.index');
     }
 
 
