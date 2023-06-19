28f9c12b11893219a8c0fe4bdc6ebae76ee78ef8
hfizhirfan <hafizh.irfansyah41@gmail.com>
2023-06-19 08:32:21 +0700

    update lagi

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -12,46 +12,53 @@ class EmployeeController extends Controller
 
     public function index()
     {
-      
+        $pageTitle = 'Employee List';
+
+        $employees = Employee::all();
+
+          return view('employee.index', [
+              'pageTitle' => $pageTitle,
+              'employees' => $employees
+          ]);
 
     }
 
 
     public function create()
     {
-        
+
 
     }
 
 
     public function store(Request $request)
     {
-        
+
     }
 
 
     public function show(string $id)
     {
-        
+
 
     }
 
     public function edit(string $id)
     {
-        
+
 
     }
 
 
     public function update(Request $request, string $id)
     {
-        
+
     }
 
-  
+
     public function destroy(string $id)
     {
-    
+
 
     }
 }
