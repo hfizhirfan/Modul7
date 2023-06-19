1b7254bad88ae59def063120bf4f7e7c2973313d
hfizhirfan <hafizh.irfansyah41@gmail.com>
2023-06-19 09:10:53 +0700

    update index

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -13,6 +13,14 @@ class EmployeeController extends Controller
 
     public function index()
     {
+        $pageTitle = 'Employee List';
+
+        $employees = Employee::all();
+
+          return view('employee.index', [
+              'pageTitle' => $pageTitle,
+              'employees' => $employees
+          ]);
     }
 
 
