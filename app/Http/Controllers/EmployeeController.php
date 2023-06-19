2bae5edd06ceb60f1ecaf01e24724e9c6282a6f2
hfizhirfan <hafizh.irfansyah41@gmail.com>
2023-06-19 08:48:03 +0700

    update indexnya

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -20,45 +21,111 @@ class EmployeeController extends Controller
               'pageTitle' => $pageTitle,
               'employees' => $employees
           ]);
-
     }
 
 
     public function create()
     {
+        $pageTitle = 'Create Employee';
 
+        // Eloquent
+        $positions = Position::all();
 
+        return view('employee.create', compact('pageTitle', 'positions'));
     }
 
 
     public function store(Request $request)
     {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
 
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email|unique:employees,email,',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // Eloquent
+        $employee = new Employee;
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+        return redirect()->route('employees.index');
     }
 
 
     public function show(string $id)
     {
+        $pageTitle = 'Employee Detail';
 
+        // ELOQUENT
+        $employee = Employee::find($id);
 
+        return view('employee.show', compact('pageTitle', 'employee'));
     }
 
     public function edit(string $id)
     {
+        $pageTitle = 'Edit Employee';
 
+        //Eloquent
+        $positions = Position::all();
+        $employee = Employee::find($id);
 
+        return view('employee.edit', compact('pageTitle', 'positions', 'employee'));
     }
 
 
     public function update(Request $request, string $id)
     {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
 
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // ELOQUENT
+        $employee = Employee::find($id);
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+        return redirect()->route('employees.index');
     }
 
 
     public function destroy(string $id)
     {
+        // ELOQUENT
+        Employee::find($id)->delete();
 
-
+        return redirect()->route('employees.index');
     }
 }
