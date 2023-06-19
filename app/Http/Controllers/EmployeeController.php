9007b50bf913f4ebde27a4cdfb89e0ed70cbbae6
rzqthya <athiyya.fan178@gmail.com>
2023-06-19 08:47:37 +0700

    update fitur list

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -12,8 +12,15 @@ class EmployeeController extends Controller
 
     public function index()
     {
-    
+        $pageTitle = 'Employee List';
 
+        // Eloquent
+        $employees = Employee::all();
+
+        return view('employee.index', [
+            'pageTitle' => $pageTitle,
+            'employees' => $employees
+        ]);
     }
 
 
